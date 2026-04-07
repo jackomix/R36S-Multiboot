1b9d74bc6ede5109c4c8113acb6b7e5dd43fccea
jackomix <jacklack33@outlook.com>
2026-04-07 00:56:04 -0400

    Fix syntax error in ark/install-os.sh by quoting say commands

--- ark/install-os.sh
+++ ark/install-os.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
-say Installing dArkOSre (as ark) to ${imgname}
+say "Installing dArkOSre (as ark) to ${imgname}"
 
 npsz=$(cat bootsizereq)
-sayin new $((npsz/1024))GiB partition
+sayin "new $((npsz/1024))GiB partition"
 newpart $npsz fat arkboot
 ThisBootPartNum=${partcount}
 BootDev=${LATEST_PART_DEV}
-sayin new dev is ${BootDev}
+sayin "new dev is ${BootDev}"
 
 npsz=$(cat sizereq)
-sayin new $((npsz/1024))GiB partition
+sayin "new $((npsz/1024))GiB partition"
 newpart $npsz ext4 ark
 ThisRootPartNum=${partcount}
 RootDev=${LATEST_PART_DEV}
-sayin new dev is ${RootDev}
+sayin "new dev is ${RootDev}"
 
-sayin setup mounts
+sayin "setup mounts"
 arkBootMnt=${tmpmnts}/boot
 arkRootMnt=${tmpmnts}/root
 [[ ! -d "${arkBootMnt}" ]] && mkdir -p "${arkBootMnt}"
@@ -31,24 +31,24 @@ sudo mount ${arklodev}p2 "${arkRootMnt}" || exit 1
 sync
 
 BootDestMnt=${tmpmnts}/${imgname}-arkboot
-sayin copy boot files to ${BootDestMnt}
+sayin "copy boot files to ${BootDestMnt}"
 mkdir -p "${BootDestMnt}" || exit 1
 sudo mount ${BootDev} "${BootDestMnt}" || exit 1
-sayin copy boot to ${imgname}
+sayin "copy boot to ${imgname}"
 sudo rsync -aHAX --no-compress ${arkBootMnt}/ ${BootDestMnt} >/dev/null 2>&1
 sudo mkdir -p ${BootDestMnt}/u-boot
 sync
 
-sayin copy boot files to ${ImgBootMnt}
+sayin "copy boot files to ${ImgBootMnt}"
 sudo cp -R "boot.ark.ini" "${ImgBootMnt}/"
 ThisBootPartNumHex=$(printf '%x\n' ${ThisBootPartNum})
 sudo sed -i "s|###bootPartNum###|${ThisBootPartNumHex}|g" "${ImgBootMnt}/boot.ark.ini"
 
 RootDestMnt=${tmpmnts}/${imgname}-ark
-sayin mount ${RootDev} "${RootDestMnt}"
+sayin "mount ${RootDev} ${RootDestMnt}"
 mkdir -p "${RootDestMnt}" || exit 1
 sudo mount ${RootDev} "${RootDestMnt}" || exit 1
-sayin copy root to ${imgname}
+sayin "copy root to ${imgname}"
 sudo rsync -aHAX --no-compress ${arkRootMnt}/ ${RootDestMnt} >/dev/null 2>&1
 sync
 
